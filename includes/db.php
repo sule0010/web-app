9731f9f9a1ed2c76b77e94d52304543ba0e23012
sule0010 <bashir.b.suleiman@gmail.com>
2012-08-08 13:51:09 -0400

    changed db to phpfog.

--- includes/db.php
+++ includes/db.php
@@ -5,9 +5,12 @@
 
 // Our username and password are kept in Environment variables, in .htaccess
 // This is for security, so they are never publicly visible in Github
-$user = getenv('DB_USER');       // the MySQL username
-$pass = getenv('DB_PASS');           // The MySQL password
-$data_source = getenv('DATA_SOURCE');
+$user = getenv('MYSQL_USERNAME');       // the MySQL username
+$pass = getenv('MYSQL_PASSWORD');           // The MySQL password
+$host = getenv('MYSQL_DB_HOST');
+$dbname = getenv('MYSQL_DB_NAME');
+
+$data_source = getenv('mysql:host=%s;dbname=%s' $host, $dbname);
 
 // PDO: PHP Data Objects
 // Allows us to connect to many different kinds of databases
